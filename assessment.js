5530c6e6396a602e85fea28b42a45d37c30bc187
生駒 葉月 <ikoma.haduki86@gmail.com>
2025-11-12 15:43:16 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -16,20 +16,32 @@ assessmentButton.addEventListener( //イベント検知設定を追加
     console.log(assessment(userName)); // ログ出力を確認
     // 診断結果エリアの作成
     resultDivision.innerText = '';
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
 
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision をresultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // X投稿ボタンの作成
     tweetDivision.innerText = '';
     const anchor = document.createElement('a');// aタグの作成
-    const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' +  encodeURI('あなたのいいところ') + '&ref_src=twsrc%5Etfw';
+    const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=' + encodeURI(userName + 'のいいところ') + '&ref_src=twsrc%5Etfw';
 
 
     anchor.setAttribute('href', hrefValue);// 属性hrefを追加
